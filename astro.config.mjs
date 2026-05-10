c6a479cb4081215e7c4940d1275341f7170fa11c
Mo <muhamedw@gmail.com>
2026-05-10 12:52:25 -0400

    feat: scaffold Astro project with Tailwind and Sitemap

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+// astro.config.mjs
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
+
+export default defineConfig({
+  site: 'https://wisetech.ca',
+  integrations: [
+    tailwind(),
+    sitemap(),
+  ],
+  output: 'static',
+});
